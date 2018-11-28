bc164f74230ec2f2b47e09a293eb139788ab117d
Oleksii Shmyha <oleksii_shmyha@epam.com>
2018-11-28 12:50:46 +0200

    Added Percolation Stats main method feature definition for future construction.

--- src/main/java/PercolationStats.java
+++ src/main/java/PercolationStats.java
@@ -1,7 +1,8 @@
 public class PercolationStats {
     /**
      * perform trials independent experiments on an n-by-n grid
-     * @param n size of grid
+     *
+     * @param n      size of grid
      * @param trials the number of experiment trials
      */
     public PercolationStats(int n, int trials) {
@@ -37,10 +38,23 @@ public class PercolationStats {
     }
 
     public static void main(String[] args) {
+        /*
+        takes two command-line arguments n and T,
+        performs T independent computational experiments (discussed above) on an n-by-n grid,
+        and prints the sample mean, sample standard deviation, and the 95% confidence interval for the percolation threshold
+        Use StdRandom to generate random numbers;
+        use StdStats to compute the sample mean and sample standard deviation.
+
+        Example
+        % java-algs4 PercolationStats 200 100
+        mean                    = 0.5929934999999997
+        stddev                  = 0.00876990421552567
+        95% confidence interval = [0.5912745987737567, 0.5947124012262428]
+         */
     }
 
     private void validateInputs(int n, int trials) {
-        if(n < 1 || trials < 1) {
+        if (n < 1 || trials < 1) {
             throw new IllegalArgumentException("n and trials params must be grater than 0");
         }
     }
